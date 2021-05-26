5b1b74e72c73b45ceb7602e00e0c6bce580b1c96
Famosito <reynaldocruz07@hotmail.com>
2021-05-26 08:58:43 +0800

    App secreto

---
+++ Data/UserServices.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tarea5.Data
+{
+    public class UserServices
+    {
+        private SecretsDbContext dbContext;
+        public UserServices(SecretsDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        //get
+        public async Task<List<User>> GetUsersAsync()
+        {
+            return await dbContext.Users.ToListAsync();
+        }
+
+        //add
+        public async Task<User> AddUserAsync(User user)
+        {
+            try
+            {
+                dbContext.Users.Add(user);
+                await dbContext.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return user;
+        }
+
+        //update
+        public async Task<User> UpdateUserAsync(User user)
+        {
+            try
+            {
+                var userExist = dbContext.Users.FirstOrDefault(p => p.UserId == user.UserId);
+                if (userExist != null)
+                {
+                    dbContext.Users.Update(user);
+                    await dbContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return user;
+        }
+    }
+}
